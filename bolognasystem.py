e94e79819fad4328f95a4cbf062e78b8a234742f
resadhusenli <resad0qurbanov@gmail.com>
2019-11-29 02:09:07 +0400

    bolognasystem.py

---
+++ bolognasystem.py
@@ -0,0 +1,10 @@
+import random
+a=random.randrange(0,100)
+if(100>=a) and(a>=90):
+    print(a,"A seviyye")
+elif(89>=a) and(a>=80):
+    print(a,"B seviyye")
+elif(79>=a) and(a>=70):
+    print(a,"C seviyye")
+else:
+    print(a,"F seviyye")
