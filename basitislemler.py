71ab62790a9b0e235d1f0bac166fb7264a34e44d
resadhusenli <resad0qurbanov@gmail.com>
2019-11-29 02:04:43 +0400

    basitislemler.py

---
+++ basitislemler.py
@@ -0,0 +1,7 @@
+a=5
+b=2
+print(a,"-nin ve",b,"-nin toplami",a+b)
+print(a,"-nin ve",b,"-nin farki",a-b)
+print(a,"-nin ve",b,"-nin carpimi",a*b)
+print(a,"-nin ve",b,"-nin bolumu",a/b)
+print(a,"-nin ve",b,"-nin bolumunden kalan",a%b)
